2e41fb2def8156059c7a8b20fe4cf621f041487d
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-03-13 19:21:30 +0300

    Bugfix

--- src/js/themes.js
+++ src/js/themes.js
@@ -5,7 +5,6 @@ export const setNightMode = function () {
     'd',
     'M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z'
   );
-  isDarkModeActive = true;
   document.querySelector('body').style.backgroundColor = '#111111';
   document.querySelector('.toolbar-icons').style.color = '#cccccc';
   document.querySelector('.github a').style.fill = '#cccccc';
@@ -21,7 +20,6 @@ export const setDayMode = function () {
     'd',
     'M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'
   );
-  isDarkModeActive = false;
   document.querySelector('body').style.backgroundColor = '#ffffff';
   document.querySelector('.toolbar-icons').style.color = '#555555';
   document.querySelector('.github a').style.fill = '#555555';
