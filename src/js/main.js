83f341a67f43de3869db46f75b2f192c6f5ca744
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-03-15 18:18:56 +0300

    implementing details page --dev-4

--- src/js/main.js
+++ src/js/main.js
@@ -16,10 +16,12 @@ const detailsCurrencies = document.querySelector('.details-currencies');
 const detailsIdd = document.querySelector('.details-idd');
 const detailsIndependent = document.querySelector('.details-independent');
 const detailsLandlocked = document.querySelector('.details-landlocked');
+const detailsLanguages = document.querySelector('.details-languages');
 const input = document.querySelector('.search-input');
 const search = document.querySelector('.search-btn');
+const showAllTextKeyword = document.querySelector('.show-all-text-keyword');
 const toggler = document.querySelector('.night-mode');
-const goBack = document.querySelector('.show-all-text span');
+const goBack = document.querySelector('.show-all-text-click');
 
 let isDarkModeActive = false;
 
@@ -27,14 +29,12 @@ const getData = async function (keyword) {
   const res = await fetch(`https://restcountries.com/v3.1/${keyword}`);
   const data = await res.json();
   // console.log(data);
-  // console.log(data[0]);
+  // console.log(data[95]);
   return data;
 };
 
 const renderCountries = function (data) {
-  while (listCards.hasChildNodes()) {
-    listCards.removeChild(listCards.firstChild);
-  }
+  itemCleaner(listCards);
 
   data.forEach(item => {
     const cardContent = `
@@ -70,43 +70,75 @@ const renderDetails = function (data) {
   scroll(0, 0);
   data = data[0];
   const flag = `<img src="${data.flags.svg}" alt="${data.demonyms.eng.m} flag" style="width: 12rem; margin-top: 0.4rem; border-radius: 1rem" />`;
-  detailsFlag.insertAdjacentHTML('beforeend', flag);
-  detailsAltSpellings.insertAdjacentHTML(
-    'beforeend',
+  const arms = `<img src="${data.coatOfArms.svg}" alt="${data.demonyms.eng.m} arms" style="width: 12rem; margin-top: 0.4rem"; border-radius: 1rem>`;
+
+  insertData(detailsFlag, flag);
+  insertData(
+    detailsAltSpellings,
     data.altSpellings.toString().replaceAll(',', ', ')
   );
-  detailsBorders.insertAdjacentHTML(
-    'beforeend',
+  insertData(
+    detailsBorders,
     data.borders ? data.borders.toString().replaceAll(',', ', ') : 'No borders'
   );
-  detailsCapital.insertAdjacentHTML(
-    'beforeend',
-    data.capital.toString().replaceAll(',', ', ')
-  );
-  detailsCarDirection.insertAdjacentHTML(
-    'beforeend',
+  insertData(detailsCapital, data.capital.toString().replaceAll(',', ', '));
+  insertData(
+    detailsCarDirection,
     data.car.side.toString()[0].toUpperCase() + data.car.side.slice(1)
   );
-  const arms = `<img src="${data.coatOfArms.svg}" alt="${data.demonyms.eng.m} arms" style="width: 12rem; margin-top: 0.4rem"; border-radius: 1rem>`;
-  detailsCoatOfArms.insertAdjacentHTML('beforeend', arms);
-  detailsContinents.insertAdjacentHTML(
-    'beforeend',
+  insertData(detailsCoatOfArms, arms);
+  insertData(
+    detailsContinents,
     data.continents.toString().replaceAll(',', ', ')
   );
-  const cur = data.currencies;
-  detailsCurrencies.insertAdjacentHTML(
-    'beforeend',
-    `${cur[Object.keys(cur)[0]].name} (${cur[Object.keys(cur)[0]].symbol})`
-  );
-  detailsIdd.insertAdjacentHTML('beforeend', data.idd.root);
-  detailsIndependent.insertAdjacentHTML(
-    'beforeend',
-    data.independent ? 'Yes' : 'No'
-  );
-  detailsLandlocked.insertAdjacentHTML(
-    'beforeend',
-    data.landlocked ? 'Yes' : 'No'
-  );
+  for (const [_, value] of Object.entries(data.currencies)) {
+    let currencies = '';
+    currencies += `${value.name} (${value.symbol}), `;
+    insertData(detailsCurrencies, currencies);
+  }
+  insertData(detailsIdd, data.idd.root);
+  insertData(detailsIndependent, data.independent ? 'Yes' : 'No');
+  insertData(detailsLandlocked, data.landlocked ? 'Yes' : 'No');
+  for (const [_, value] of Object.entries(data.languages)) {
+    let languages = '';
+    languages += `${value}, `;
+    insertData(detailsLanguages, languages);
+  }
+  // detailsFlag.insertAdjacentHTML('beforeend', flag);
+  // detailsAltSpellings.insertAdjacentHTML(
+  //   'beforeend',
+  //   data.altSpellings.toString().replaceAll(',', ', ')
+  // );
+  // detailsBorders.insertAdjacentHTML(
+  //   'beforeend',
+  //   data.borders ? data.borders.toString().replaceAll(',', ', ') : 'No borders'
+  // );
+  // detailsCapital.insertAdjacentHTML(
+  //   'beforeend',
+  //   data.capital.toString().replaceAll(',', ', ')
+  // );
+  // detailsCarDirection.insertAdjacentHTML(
+  //   'beforeend',
+  //   data.car.side.toString()[0].toUpperCase() + data.car.side.slice(1)
+  // );
+  // detailsCoatOfArms.insertAdjacentHTML('beforeend', arms);
+  // detailsContinents.insertAdjacentHTML(
+  //   'beforeend',
+  //   data.continents.toString().replaceAll(',', ', ')
+  // );
+  // detailsCurrencies.insertAdjacentHTML(
+  //   'beforeend',
+  //   `${cur[Object.keys(cur)[0]].name} (${cur[Object.keys(cur)[0]].symbol})`
+  // );
+  // detailsIdd.insertAdjacentHTML('beforeend', data.idd.root);
+  // detailsIndependent.insertAdjacentHTML(
+  //   'beforeend',
+  //   data.independent ? 'Yes' : 'No'
+  // );
+  // detailsLandlocked.insertAdjacentHTML(
+  //   'beforeend',
+  //   data.landlocked ? 'Yes' : 'No'
+  // );
 };
 
 search.addEventListener('click', async function () {
@@ -114,6 +146,9 @@ search.addEventListener('click', async function () {
     const data = await getData(`name/${input.value}`);
     renderCountries(data);
     document.querySelector('.show-all').style.display = 'block';
+    itemCleaner(showAllTextKeyword);
+    showAllTextKeyword.insertAdjacentHTML('beforeend', input.value);
+    input.value = '';
   }
 });
 
@@ -153,11 +188,8 @@ detailsExit.addEventListener('click', () => {
     detailsIdd,
     detailsIndependent,
     detailsLandlocked,
-  ].forEach(domItem => {
-    while (domItem.hasChildNodes()) {
-      domItem.removeChild(domItem.firstChild);
-    }
-  });
+    detailsLanguages,
+  ].forEach(domItem => itemCleaner(domItem));
 
   details.style.display = 'none';
 });
@@ -173,3 +205,11 @@ const init = async function () {
 };
 
 init();
+
+const itemCleaner = parentItem => {
+  while (parentItem.hasChildNodes())
+    parentItem.removeChild(parentItem.firstChild);
+};
+
+const insertData = (element, action) =>
+  element.insertAdjacentHTML('beforeend', action);
