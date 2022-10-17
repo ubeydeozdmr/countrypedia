f11148a1f339a4e9cf2bbe97b412cec51e86461d
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-10-17 22:11:01 +0300

    Fixed API routes

--- src/js/config.js
+++ src/js/config.js
@@ -1,15 +1,15 @@
 export const API_ROUTE = 'https://restcountries.com/v3.1/';
 export const API_ROUTE_ALL = 'https://restcountries.com/v3.1/all';
-export const API_ROUTE_NAME = 'https://restcountries.com/v3.1/name/';
-export const API_ROUTE_TRANSLATION = 'https://restcountries.com/v3.1/translation/';
 export const API_ROUTE_CAPITAL = 'https://restcountries.com/v3.1/capital/';
-export const API_ROUTE_REGION = 'https://restcountries.com/v3.1/region/';
-export const API_ROUTE_CURRENCY = 'https://restcountries.com/v3.1/currency/';
-export const API_ROUTE_LANG = 'https://restcountries.com/v3.1/lang/';
-export const API_ROUTE_CALLINGCODE = 'https://restcountries.com/v3.1/callingcode/';
-export const API_ROUTE_ALTSPELLING = 'https://restcountries.com/v3.1/alt-spelling/';
 export const API_ROUTE_CODE = 'https://restcountries.com/v3.1/alpha/';
 export const API_ROUTE_CODELIST = 'https://restcountries.com/v3.1/alpha?codes=';
+export const API_ROUTE_CURRENCY = 'https://restcountries.com/v3.1/currency/';
+export const API_ROUTE_DEMONYM = 'https://restcountries.com/v3.1/demonym/';
+export const API_ROUTE_LANG = 'https://restcountries.com/v3.1/lang/';
+export const API_ROUTE_NAME = 'https://restcountries.com/v3.1/name/';
+export const API_ROUTE_REGION = 'https://restcountries.com/v3.1/region/';
+export const API_ROUTE_SUBREGION = 'https://restcountries.com/v3.1/subregion/';
+export const API_ROUTE_TRANSLATION = 'https://restcountries.com/v3.1/translation/';
 
 // prettier-ignore
 export const ERROR_NOT_FOUND = "&#9888 We couldn't find what you're looking for. Try another query.";
