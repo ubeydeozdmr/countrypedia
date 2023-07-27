329565e766fe8facc8974f0ff0f9d94a9e7b2e99
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-07-28 01:56:08 +0300

    "var" types has been changed to "const"

--- src/js/views/detailsView.js
+++ src/js/views/detailsView.js
@@ -175,8 +175,8 @@ class DetailsView extends View {
     const markup = `<div id="map" class="map"></div>`;
     insert(document.querySelector('.details__content'), markup);
 
-    var map = L.map('map').setView([data.latlng[0], data.latlng[1]], 5);
-    var marker = L.marker([
+    const map = L.map('map').setView([data.latlng[0], data.latlng[1]], 5);
+    const marker = L.marker([
       data.capitalInfo.latlng ? data.capitalInfo.latlng[0] : data.latlng[0],
       data.capitalInfo.latlng ? data.capitalInfo.latlng[1] : data.latlng[1],
     ]).addTo(map);
